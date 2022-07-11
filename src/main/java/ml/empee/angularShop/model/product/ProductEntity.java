e73a7a8996e8a90649c53a00c3abf60c3f0d95c0
Mr-EmPee <manuel.palumbo@email.com>
2022-07-11 13:02:52 +0200

    refactoring

--- src/main/java/ml/empee/angularShop/model/product/ProductEntity.java
+++ src/main/java/ml/empee/angularShop/model/product/ProductEntity.java
@@ -2,40 +2,34 @@ package ml.empee.angularShop.model.product;
 
 import lombok.Getter;
 import lombok.Setter;
+import ml.empee.angularShop.model.category.CategoryEntity;
 
 import javax.persistence.*;
 
-@Entity
+@Entity @Getter @Setter
 @Table(name = "products")
 public class ProductEntity {
 
-    @Id @Getter @Setter
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CUST_SEQ")
-    @SequenceGenerator(sequenceName = "prod_seq", allocationSize = 1, name = "CUST_SEQ")
+    @Id @GeneratedValue
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Getter @Setter
     @Column(name = "title", nullable = false)
     private String title;
 
-    @Getter @Setter
     @Column(name = "description", nullable = false)
     private String description;
 
-    @Getter @Setter
     @Column(name = "size", nullable = false)
     private String size;
 
-    @Getter @Setter
     @Column(name = "price", nullable = false)
     private Double price;
 
-    @Getter @Setter
-    @Column(name = "category", nullable = false)
-    private String category; //TODO maybe an enum?
+    @ManyToOne()
+    @JoinColumn(name="category_id", nullable=false)
+    private CategoryEntity category;
 
-    @Getter @Setter
     @Column(name = "image_path")
     private String imagePath;
 
